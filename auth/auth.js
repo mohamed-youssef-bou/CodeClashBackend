32d6546c166261b55bab77177eff9d9fd6a5ea78
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-10-14 18:54:49 -0400

    Adding authentication functionality with Passport (login implemented and secure routing example included)

---
+++ auth/auth.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const controller = require('./../mongo/controller');
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+passport.use('login', new LocalStrategy(
+    {
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    async (username, password, done) => {
+        try {
+            const [user, validate] = await controller.validatePassword(username, password)
+
+            if (!user) {
+                return done(null, false, {message: "No account exists with those credentials"});
+            } else if (!validate) {
+                return done(null, false, {message: "The password you entered is incorrect"});
+            } else {
+                return done(null, user, {message: "Successfully logged in"});
+            }
+
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.use(
+    new JWTstrategy(
+        {
+            secretOrKey: 'TOP_SECRET',
+            jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+        },
+        async (token, done) => {
+            try {
+                return done(null, token.user);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
--- mongo/controller.js
+++ mongo/controller.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 // const mongoose = require('mongoose');
 const ObjectId = require('mongodb').ObjectId;
+const bcrypt = require('bcrypt');
 
 const credentials = require('./credentials');
 
@@ -10,11 +11,11 @@ const databaseName = "CodeClash";
 module.exports = {
 
     // All connections are asynchronous
-    test: async function(input){
+    test: async function (input) {
 
-        var connection = await MongoClient.connect(credentials.getMongoUri(), { useUnifiedTopology: true }).catch((error) => console.log(error));
+        var connection = await MongoClient.connect(credentials.getMongoUri(), {useUnifiedTopology: true}).catch((error) => console.log(error));
         var database = connection.db(databaseName);
-        
+
         response = await database.collection("users").insertOne({name: input});
 
         connection.close();
@@ -23,4 +24,19 @@ module.exports = {
         return response.ops[0]._id;
     },
 
+    validatePassword: async function (username, passwordCandidate) {
+        let connection = await MongoClient.connect(credentials.getMongoUri(), {useUnifiedTopology: true}).catch((error) => console.log(error));
+        let database = connection.db(databaseName);
+
+        const user = await database.users.findOne({name: username}).catch((error) => console.log(error));
+        await connection.close();
+
+        if (!user) {
+            return [null, false];
+        }
+
+        const validate = await bcrypt.compare(passwordCandidate, user.password);
+
+        return [user, validate];
+    }
 }
