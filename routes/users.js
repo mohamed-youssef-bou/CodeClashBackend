207c7545f17cadc784bd2ddd5049aae828b917a5
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-10-16 20:12:38 -0400

    created API method for UpdateUserInfo

--- routes/users.js
+++ routes/users.js
@@ -1,66 +1,88 @@
-var express = require('express');
-var create_connection = require('./../mongo/create_connection');
+var express = require("express");
+var create_connection = require("./../mongo/create_connection");
 var router = express.Router();
 var validator = require("validator");
 
-router.post('/', async function(req, res) {
+router.post("/", async function (req, res) {
   // Get values from request
-  var username = req.headers['username'];
-  var email = req.headers['email'];
-  var password = req.headers['password'];
+  var username = req.headers["username"];
+  var email = req.headers["email"];
+  var password = req.headers["password"];
 
   //Error handling
   if (validator.isEmpty(username)) {
-    return res.status(400).send('Empty username');
-  } 
-  else if (!validator.matches(username, '^[a-zA-Z0-9_.-]*$')) {
-    return res.status(400).send('Invalid username');
-  } 
+    return res.status(400).send("Empty username");
+  } else if (!validator.matches(username, "^[a-zA-Z0-9_.-]*$")) {
+    return res.status(400).send("Invalid username");
+  }
 
   if (!validator.isEmail(email)) {
-    return res.status(400).send('Invalid email address');
+    return res.status(400).send("Invalid email address");
   }
 
   if (validator.isEmpty(password)) {
-    return res.status(400).send('Empty password');
-  } 
-  
+    return res.status(400).send("Empty password");
+  }
+
   var response = await create_connection.create_user(username, email, password);
   return res.status(response[0]).send(response[1]);
 });
 
-router.post('/deleteUser', async function(req, res) {
+router.post("/deleteUser", async function (req, res) {
   // Get values from request
-  var user_id = req.headers['user_id'];
-  var password = req.headers['password'];
+  var user_id = req.headers["user_id"];
+  var password = req.headers["password"];
 
   //Error handling
   if (validator.isEmpty(user_id)) {
-    return res.status(400).send('Empty user ID');
-  } 
+    return res.status(400).send("Empty user ID");
+  }
 
   if (validator.isEmpty(password)) {
-    return res.status(400).send('Empty password');
-  } 
-  
+    return res.status(400).send("Empty password");
+  }
+
   var response = await create_connection.delete_user(user_id, password);
   return res.status(response[0]).send(response[1]);
 });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/:id', async function(req, res, next) {
-  if(req.params.id == null) {
-      res.status(400).send({ error: "_id cannot be null"});
-      return;
+router.get("/:id", async function (req, res, next) {
+  if (req.params.id == null) {
+    res.status(400).send({ error: "_id cannot be null" });
+    return;
   }
 
   let response = await create_connection.getUserById(req.params.id, res);
   res.status(response[0]).send(response[1]);
-  
+});
+
+/* Update User Info */
+router.put("/updateUser", async function (req, res) {
+  // Get values from request
+  var user_id = req.headers["user_id"];
+  var new_username = req.headers["new_username"];
+  var new_password = req.headers["new_password"];
+
+  //Error handling
+  if (validator.isEmpty(user_id)) {
+    return res.status(400).send("Empty user ID");
+  }
+
+  if (validator.isEmpty(new_username)) {
+    return res.status(400).send("Empty new username");
+  }
+
+  if (validator.isEmpty(new_password)) {
+    return res.status(400).send("Empty new password");
+  }
+
+  var response = await create_connection.updateUser(user_id, new_username, new_password);
+  return res.status(response[0]).send(response[1]);
 });
 
 module.exports = router;
