142944c60fb4f2eb77e4605985f37a41bb84579e
taylorlynn <taylor.curtis@mail.mcgill.ca>
2020-10-22 13:10:51 -0400

    Created REST get all challenges

---
+++ routes/challenges.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var create_connection = require("./../mongo/create_connection");
+var router = express.Router();
+var validator = require("validator");
+
+
+/* GET users listing. */
+router.get("/challenges", function (req, res, next) {
+  let response = await create_connection.getAllActiveChallenges();
+  res.status(response[0]).send(response[1]);
+});
+
+
+module.exports = router;
